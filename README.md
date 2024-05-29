ee760615adfd1f459a75a243c0ba54db6ce3f7ce
amarsingh <amarsinghpatil31@gmail.com>
2024-05-29 14:13:41 +0530

    Updating Upto connecting database in MERN

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 this is videos series on backend with javascript - 
 - [Model Link ](https://app.eraser.io/workspace/GYV1MdorsvmrY3tCuCJo?origin=share)
+- [ ]
--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
-console.log("hello World")
 
-for(let i = 0; i <= 5; i++){
-    console.log(i);
-}
+    import dotenv from "dotenv"
+    import connectDB from "./db/index.js"
+
+    dotenv.config({
+        path: './env'
+    })
+
+connectDB()
+
+
+
+// import express from "express"
+// const app = express()
+
+// ( async () => {
+
+//     try {
+
+//         // 1. connecting to mongodb
+//       await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         // this is how an database is connected with an name given by an DB_NAME
+//         // async await is used because of an database is in another contitent
+
+
+//         // 2. error handling
+//         app.on("error", () => {
+//             console.log("ERR: ", error);
+//             throw error
+//         })
+
+//         // 3. starting the server
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.error("ERROR: ", error)
+//         throw err
+
+//         throw error means exit an proess or exit an
+//     }
+
+// }) ()
